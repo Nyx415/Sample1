7cf71fa7c68edcd5ac2fb9677f334253de241d76
Nyx415 <index0415@gmail.com>
2016-11-13 20:45:48 +0900

    Add files via upload

---
+++ exam5-2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<title>PHPテスト</title>
+</head>
+<body>
+  <?php
+// <body text='black'> となります
+$bodytag = str_replace("%body%", "black", "<body text='%body%'>");
+
+// Hll Wrld f PHP となります
+$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+$onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
+
+// You should eat pizza, beer, and ice cream every day となります
+$phrase  = "You should eat fruits, vegetables, and fiber every day.";
+$healthy = array("fruits", "vegetables", "fiber");
+$yummy   = array("pizza", "beer", "ice cream");
+
+$newphrase = str_replace($healthy, $yummy, $phrase);
+
+// 2 となります
+$str = str_replace("ll", "", "good golly miss molly!", $count);
+echo $count;
+?>
+</body>
+</html>
